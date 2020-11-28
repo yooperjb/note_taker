30cbf17d2f3d1472bf3dec81661a89aaab1a17ab
Pedal <jmb181@humboldt.edu>
2020-11-27 23:42:09 -0800

    getting post to work and added unique ID

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs'); // we need this in order to write to the json file
+const path = require('path'); // used for working with file and directory paths
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const notes = require('./db/db'); //require data
@@ -5,6 +7,11 @@ const notes = require('./db/db'); //require data
 // instantiate the server
 const app = express();
 
+// This is the necessary middlware methods executed by Express.js server that our requests pass through before getting to the intended endpoint. Both the below are required for servers that accept POST data
+// Takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object
+app.use(express.urlencoded({ extended: true }));
+// Takes incoming POST data in the form of JSON and parses into req.body JavaScript object
+app.use(express.json());
 
 // not sure if this is needed or not
 function filterByQuery(query, notesArray) {
@@ -16,12 +23,31 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 }
 
+// get note by the id
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+};
+
+// create new note from the POST body to the note array
+function createNewNote(body, notesArray) {
+    
+    // get data response from POST as body
+    const note = body;
+    // push new note to notes array
+    notesArray.push(note);
+
+    return note;
+};
+
 // route to data. First argument is string of route client has to fetch from. Second arg is callback that will execute everytime that route is accesses with GET request. 
 app.get('/api/notes', (req, res) => {
     
     let results = notes;
+    //console.log("req.body is: ", typeof req.body);
+    //console.log(req.body);
 
-    // if query parameter used filter results
+    // if query parameter property used filter results (req.query looks for any query after the ? in the URL)
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
@@ -29,9 +55,34 @@ app.get('/api/notes', (req, res) => {
     res.json(results);
 });
 
+// get route for parameter property - id
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result){
+       res.json(result); 
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+
+// post route that accepts data stored server-side
+app.post('/api/notes', (req, res) => {
+
+    // create a unique ID to add to each new note using Math.max and spread operator. req.body is the data that is received from POST
+    req.body.id = (Math.max(...notes.map(note => note.id)) +1 ).toString();
+    console.log(req.body);
+
+    // add note to json file
+    const note = createNewNote(req.body, notes);
+    
+    res.json(note);
+});
 
 // this makes the server listen
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}!`);
 });
+
+
 
